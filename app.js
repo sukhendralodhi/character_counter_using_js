8c25ffcaf2d8ed513939a3b9413edaa0cb0e1280
sukhendra <mpct5jan1998@gmail.com>
2023-02-26 23:59:32 +0530

    update v1.0

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const countEle = document.getElementById('text-holder');
 const inputEle = document.getElementById('text_input');
+const containerEle = document.getElementById('container');
 
 const charCounter = ()=> {
+
     countEle.innerText = `Count: ${inputEle.value.length}`
+
+    if(inputEle.value.length > 10) {
+        countEle.style.color = 'white';
+        containerEle.classList.add('bg-white')
+    }
 }
 
 inputEle.addEventListener('keyup', charCounter);
